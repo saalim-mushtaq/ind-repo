af25c41ec7f3831c0c53a41fcd2406162b192f71
saalim-mushtaq <saalimmushtaq744@gmail.com>
2024-04-30 16:00:25 +0530

    fix: area calculation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const baseValue = prompt('Enter the base of a triangle: ');
 const heightValue = prompt('Enter the height of a triangle: ');
 
 // calculate the area
-const areaValue = (baseValue * heightValue) / 2;
+const areaValue = (baseValue * heightValue) / 2.5;
 
 console.log(
   `The area of the triangle is ${areaValue}`
